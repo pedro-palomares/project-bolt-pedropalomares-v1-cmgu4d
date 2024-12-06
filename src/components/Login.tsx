783a8b487bb5c274e09b26f03f32b2e1cf43365d
pedro-palomares <pedro.digitalcoach@gmail.com>
2024-12-06 16:46:19 +0100

    ok

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
-import { auth } from "./firebase";
+import { auth } from "../config/firebase";
 
 const Login = () => {
   const loginWithGoogle = async () => {
