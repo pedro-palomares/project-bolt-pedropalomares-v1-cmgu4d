c9f413c874aca2f626c65a74204479923145d0d7
pedro-palomares <pedro.digitalcoach@gmail.com>
2024-12-06 15:55:13 +0100

    ok

---
+++ src/config/firebase.ts
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+// Configuración de Firebase
+const firebaseConfig = {
+  apiKey: "TU_API_KEY",
+  authDomain: "TU_AUTH_DOMAIN",
+  projectId: "TU_PROJECT_ID",
+  storageBucket: "TU_STORAGE_BUCKET",
+  messagingSenderId: "TU_MESSAGING_SENDER_ID",
+  appId: "TU_APP_ID",
+};
+
+// Inicializa Firebase
+const app = initializeApp(firebaseConfig);
+
+// Exporta los servicios para usarlos en tu proyecto
+export const auth = getAuth(app);
+export const db = getFirestore(app);
