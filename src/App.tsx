ae932b2ab0e4dd596918c732b4f90b3d67b2472c
pedro-palomares <pedro.digitalcoach@gmail.com>
2024-12-06 16:22:11 +0100

    ok

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./config/firebase";
+import Navbar from "./components/Navbar";
 import Login from "./components/Login";
 import Dashboard from "./components/Dashboard";
 
-// Se define el componente App como una función que retorna JSX.
-// Se especifica el tipo de retorno como JSX.Element para mayor claridad.
-const App: React.FC<{}> = (): JSX.Element => {
+const App: React.FC = () => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
   return (
-    <div>
-      <Login />
-      <Dashboard />
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={user ? <Dashboard /> : <Login />} />
+        <Route path="/" element={<h1>Página Principal</h1>} />
+      </Routes>
+    </Router>
   );
 };
 
