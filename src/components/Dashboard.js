0c0eef1bc6c309abe7d1e76d2c4b1b1594663121
pedro-palomares <pedro.digitalcoach@gmail.com>
2024-12-07 06:12:05 +0100

    ok

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,30 +1,30 @@
 import React from "react";
-import { auth } from "../firebase";
 import { signOut } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
+import { auth } from "../config/firebase";
+import { useAuth } from "../context/AuthContext";
 
 const Dashboard = () => {
   const navigate = useNavigate();
+  const { currentUser } = useAuth();
 
   const handleLogout = async () => {
     try {
       await signOut(auth);
       alert("Has cerrado sesión correctamente.");
-      navigate("/");
+      navigate("/login");
     } catch (error) {
       console.error("Error al cerrar sesión:", error);
       alert("Error al cerrar sesión. Por favor, inténtalo de nuevo.");
     }
   };
 
-  const user = auth.currentUser;
-
   return (
     <div style={{ textAlign: "center", marginTop: "50px" }}>
-      {user ? (
+      {currentUser ? (
         <>
-          <h1>Bienvenido, {user.displayName}</h1>
-          <p>Email: {user.email}</p>
+          <h1>Bienvenido, {currentUser.displayName}</h1>
+          <p>Email: {currentUser.email}</p>
           <button
             onClick={handleLogout}
             style={{
